02f25a476d905febdd557a7041a319a4291853b3
adithik13 <127353534+adithik13@users.noreply.github.com>
2024-04-19 00:00:56 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <fstream>
 #include <iostream>
 #include <string>
+
 
 using namespace std; // certfied std'er B)
 
 int main() {
+    std::cout << "Hello world!" << std::endl;
 
+    // this is just a starting file.
+    ifstream file("Trimmed_Crime_Data_from_2020_to_Present.csv");
+    string line;
+    getline(file, line); // Clear header
+    while (getline(file, line))
+    {
+        // Date reported
+        size_t firstDelim = line.find(',');
+        size_t secondDelim = line.find(',', firstDelim + 1);
+        string dateRptd = line.substr(firstDelim + 1, 10);
+        cout << dateRptd << " ";
+        // Date Occurred
+        string dateOCC = line.substr(secondDelim + 1, 10);
+        cout << dateOCC << " ";
+        // Time Occurred
+        size_t thirdDelim = line.find(',', secondDelim + 1);
+        string timeOCC = line.substr(thirdDelim + 1, 4);
+        cout << timeOCC << " ";
+        // Area Name
+        size_t fifthDelim = line.find(',', line.find(',', thirdDelim + 1) + 1);
+        size_t sixthDelim = line.find(',', fifthDelim + 1);
+        string areaName = line.substr(fifthDelim + 1, (sixthDelim - (fifthDelim + 1)));
+        cout << areaName << endl;
+    }
 	// create initial welcome window ! 
     sf::RenderWindow welcomeWindow(sf::VideoMode(2400, 1800), "Los Angeles Crime Visualizer");
 
